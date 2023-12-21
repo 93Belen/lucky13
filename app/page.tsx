2acf002b96e6a56076072c0617f087bf1008896e
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-12-21 14:40:18 -0600

    adding initial page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+    <div className='bg-blue absolute top-0 h-screen w-screen' >
+      <Image className='mix-blend-overlay object-cover h-full w-full object-center'  alt='' height={2000} width={2000} src={'/IMG_6059 2.jpg.jpg'} />
+    </div>
     </>
   )
 }
